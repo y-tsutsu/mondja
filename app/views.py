86e98f1a0bf50a43c721206c7f10ad16ebd92a46
tsutsu <tsutsu0928@gmail.com>
2015-08-09 03:02:01 +0900

    ログイン/ログアウト機能に対応．

--- app/views.py
+++ app/views.py
@@ -6,11 +6,14 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
 from datetime import datetime
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def home(request):
     """Renders the home page."""
     assert isinstance(request, HttpRequest)
     return render(
         request,
-        'index.html'
+        'index.html',
+        context_instance = RequestContext(request, locals())
     )
--- mondja/urls.py
+++ mondja/urls.py
@@ -12,8 +12,25 @@ from app.forms import BootstrapAuthenticationForm
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
+    # Home:
     url(r'^$', 'app.views.home', name='home'),
+
+    # Log-in:
+    url(r'^login/$',
+        'django.contrib.auth.views.login',
+        {
+            'template_name': 'login.html',
+        },
+        name = 'login'),
+
+    # Log-out:
+    url(r'^logout/$',
+        'django.contrib.auth.views.logout',
+        {
+            'template_name': 'logout.html',
+            'next_page': '../login/'
+        },
+        name = 'logout'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),

28e16a4737c2c48b57103f029e5b6917951a14c0
tsutsu <tsutsu0928@gmail.com>
2015-08-08 23:05:48 +0900

    不要なファイルを削除．

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-"""
+﻿"""
 Definition of views.
 """
 
@@ -12,38 +12,5 @@ def home(request):
     assert isinstance(request, HttpRequest)
     return render(
         request,
-        'app/index.html',
-        context_instance = RequestContext(request,
-        {
-            'title':'Home Page',
-            'year':datetime.now().year,
-        })
-    )
-
-def contact(request):
-    """Renders the contact page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/contact.html',
-        context_instance = RequestContext(request,
-        {
-            'title':'Contact',
-            'message':'Your contact page.',
-            'year':datetime.now().year,
-        })
-    )
-
-def about(request):
-    """Renders the about page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/about.html',
-        context_instance = RequestContext(request,
-        {
-            'title':'About',
-            'message':'Your application description page.',
-            'year':datetime.now().year,
-        })
+        'app/index.html'
     )

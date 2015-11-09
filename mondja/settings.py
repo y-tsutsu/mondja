05f70f410f675298b7dd92883f65f02bee7b31d9
tsutsu <tsutsu0928@gmail.com>
2015-11-10 02:24:52 +0900

    ログイン済みの状態でソーシャルログインをするとエラーになっていた対策と，githubのログインがよくなかったので削除．

--- mondja/settings.py
+++ mondja/settings.py
@@ -189,7 +189,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 AUTHENTICATION_BACKENDS = (
     'social.backends.facebook.FacebookOAuth2',
     'social.backends.twitter.TwitterOAuth',
-    'social.backends.github.GithubOAuth2',
     'django.contrib.auth.backends.ModelBackend',
 )
 
@@ -197,8 +196,6 @@ SOCIAL_AUTH_FACEBOOK_KEY = '1507860389540866'
 SOCIAL_AUTH_FACEBOOK_SECRET = '37c5826d4cf783d3a2bf5bbda51566a4'
 SOCIAL_AUTH_TWITTER_KEY = 'JpxyJFh5PXh4ovXi7MmFSkOCY'
 SOCIAL_AUTH_TWITTER_SECRET = '97eflkkQMt7MUCPAm6SgI9fRLFFFtLWkj7iFJ1Vh6wvIteL6zo'
-SOCIAL_AUTH_GITHUB_KEY = '0fe6dbc401e3b9968c46'
-SOCIAL_AUTH_GITHUB_SECRET = 'e88e408af4e6a5400105e9d94e0e0db280b6b10a'
 
 SOCIAL_AUTH_PIPELINE = (
     'social.pipeline.social_auth.social_details',

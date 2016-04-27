4ba477a04eff670c8782b4ee574436f234c540bc
tsutsu <tsutsu0928@gmail.com>
2016-04-27 19:40:06 +0900

    最新の環境でリファクタリング．

--- mondja/urls.py
+++ mondja/urls.py
@@ -3,37 +3,58 @@ Definition of urls for mondja.
 """
 
 from datetime import datetime
-from django.conf.urls import patterns, url
-from django.conf.urls import include
-from django.contrib import admin
-from django.contrib.auth import views as authviews
-from django.conf import settings
-from django.views.generic import RedirectView
+from django.conf.urls import url
+import django.contrib.auth.views
 from django.views import static
-admin.autodiscover()
+from django.views.generic import RedirectView
 
 from mondja import settings
 from mondja import dumpdata
-from app import views as appviews
+
+import app.forms
+import app.views
+
+# Uncomment the next lines to enable the admin:
+from django.conf.urls import include
+from django.contrib import admin
+from os import name
+admin.autodiscover()
 
 urlpatterns = [
     # Home:
-    url(r'^$', appviews.home, name='home'),
+    url(r'^$', app.views.home, name='home'),
 
      # Add:
-    url(r'^add/$', appviews.add_memo, name = 'add_memo'),
+    url(r'^add/$', app.views.add_memo, name = 'add_memo'),
 
     # Edit:
-    url(r'^edit/(?P<id>.*)/$', appviews.edit_memo, name = 'edit_memo'),
+    url(r'^edit/(?P<id>.*)/$', app.views.edit_memo, name = 'edit_memo'),
 
     # Delete:
-    url(r'^delete/(?P<id>.*)/$', appviews.delete_memo, name = 'delete_memo'),
+    url(r'^delete/(?P<id>.*)/$', app.views.delete_memo, name = 'delete_memo'),
 
     # Refresh:
-    url(r'^refresh/$', appviews.refresh_memo, name = 'refresh_memo'),
+    url(r'^refresh/$', app.views.refresh_memo, name = 'refresh_memo'),
 
     # Dumpdata:
     url(r'^dumpdata/(?P<app_name>.*)/$', dumpdata.dumpdata_app, name = 'dumpdata_app'),
+
+    # Log-in:
+    url(r'^login/$',
+        django.contrib.auth.views.login,
+        {
+            'template_name': 'login.html',
+        },
+        name = 'login'),
+
+    # Log-out:
+    url(r'^logout/$',
+        django.contrib.auth.views.logout,
+        {
+            'template_name': 'logout.html',
+            'next_page': '../login/'
+        },
+        name = 'logout'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
@@ -46,23 +67,6 @@ urlpatterns = [
 
     # favicon
     url(r'^favicon\.ico$', RedirectView.as_view(url = '/static/images/favicon.ico')),
-
-    # Log-in:
-    url(r'^login/$',
-        authviews.login,
-        {
-            'template_name': 'login.html',
-        },
-        name = 'login'),
-
-    # Log-out:
-    url(r'^logout/$',
-        authviews.logout,
-        {
-            'template_name': 'logout.html',
-            'next_page': '../login/'
-        },
-        name = 'logout'),
 
     # python social auth
     url(r'', include('social.apps.django_app.urls', namespace='social')),

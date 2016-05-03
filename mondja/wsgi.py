2e8f44b59b2f0fde66ac57031a9f2394f9257f4f
tsutsu <tsutsu0928@gmail.com>
2016-05-03 22:00:45 +0900

    django1.9のsettings.pyの記述方式に更新とWhiteNoiseに変更．

--- mondja/wsgi.py
+++ mondja/wsgi.py
@@ -21,8 +21,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mondja.settings")
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-application = Cling(get_wsgi_application())
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(get_wsgi_application())
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication

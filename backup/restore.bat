bcaa1ca435747acd866f7fc1d003e183cc78b65d
tsutsu <tsutsu0928@gmail.com>
2017-02-03 22:50:43 +0900

    バックアップファイルを更新．

---
+++ backup/restore.bat
@@ -0,0 +1,4 @@
+call heroku run python manage.py migrate
+call heroku run python manage.py loaddata ./backup/auth.json
+call heroku run python manage.py loaddata ./backup/social_django.json
+call heroku run python manage.py loaddata ./backup/app.json

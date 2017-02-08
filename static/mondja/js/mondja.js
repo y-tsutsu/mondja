9b379da8bca66fce516b263da8d83cd54de2cd56
tsutsu <tsutsu0928@gmail.com>
2017-02-08 23:59:24 +0900

    ダイアログでautofocusが効いていない問題を修正．
    http://stackoverflow.com/questions/35758658/bootstrap-modal-focus-not-working

--- static/mondja/js/mondja.js
+++ static/mondja/js/mondja.js
@@ -1,6 +1,12 @@
+// Buttonの連打対応
 $('form').submit(function () {
     $(this).submit(function () {
         //alert('二度押し防止！！');
         return false;
     });
+});
+
+// Every time a modal is shown, if it has an autofocus element, focus on it.
+$('.modal').on('shown.bs.modal', function () {
+    $(this).find('[autofocus]').focus();
 });

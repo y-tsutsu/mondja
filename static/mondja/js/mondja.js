37d5a75a0a4965e3ca0f40a57dc29a05e6dd405c
tsutsu <tsutsu0928@gmail.com>
2018-03-25 09:05:53 +0900

    Topにスクロールするボタンを改善．

--- static/mondja/js/mondja.js
+++ static/mondja/js/mondja.js
@@ -6,6 +6,27 @@ $('form').submit(function () {
     });
 });
 
+// トップへのスクロールボタン
+$(function () {
+    var topBtn = $('#totop > a');
+    topBtn.hide();
+    // スクロールしたらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    // スクロールしてトップ
+    topBtn.click(function () {
+        $('body, html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
+
 // Every time a modal is shown, if it has an autofocus element, focus on it.
 $('.modal').on('shown.bs.modal', function () {
     $(this).find('[autofocus]').focus();

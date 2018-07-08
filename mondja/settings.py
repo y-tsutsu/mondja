d9c37de7faabedaf712d98216676a3fca4b35715
tsutsu <tsutsu0928@gmail.com>
2018-07-08 13:44:56 +0900

    herokuにデプロイする構成を最新のものに更新．

--- mondja/settings.py
+++ mondja/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
-import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -27,7 +27,7 @@ SECRET_KEY = 'i-9%sr7zwg^awkzf(!kuk7wi-v!n*(bj@!5$jsiykttw-#4x*9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == '1'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -87,7 +87,10 @@ WSGI_APPLICATION = 'mondja.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
@@ -128,8 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles').replace(os.path.sep, '/')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static').replace(os.path.sep, '/')
@@ -179,3 +180,6 @@ PAGINATION_SETTINGS = {
     'MARGIN_PAGES_DISPLAYED': 2,
     'SHOW_FIRST_PAGE_WHEN_INVALID': True,
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals(), test_runner=False)
